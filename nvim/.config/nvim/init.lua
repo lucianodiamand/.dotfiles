a22cc1d0d0cf5e1c32915405e0ced25a90f17b91
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2023-08-26 13:57:02 -0300

    Refactor packer install

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,5 @@
 require('base')
 require('highlights')
 require('remaps')
-
+require('plugins')
 require('linux')
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,17 +1,17 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
+local success, packer = pcall(require, 'packer')
+if (not success) then
+  print('Packer is not installed')
+  return
+end
+
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
+return packer.startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.2',
-    -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
 
 end)
 
