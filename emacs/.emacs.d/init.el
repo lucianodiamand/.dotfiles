3f33353a4d8a0c4aa0ed8052ed687c40acaf9d96
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2024-06-20 11:07:19 -0300

    Add emacs config

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,18 @@
+;; Disable start up logo
+(setq inhibit-startup-message t)
+
+;; Disable visible scroolbar
+(scroll-bar-mode -1)
+;; Disable the toolbar
+(tool-bar-mode -1)
+;; Disable tooltips
+(tooltip-mode -1)
+;; Give some breathing room
+(set-fringe-mode 10)
+
+;; Disable the menu bar
+(menu-bar-mode -1)
+
+;; Set up the visible bell
+(setq visible-bell t)
+
