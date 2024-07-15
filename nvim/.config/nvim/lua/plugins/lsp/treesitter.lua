a1d04086111a3d95dc6b5db85175371166085bc6
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2024-07-15 17:28:45 -0300

    Add java support

--- nvim/.config/nvim/lua/plugins/lsp/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/lsp/treesitter.lua
@@ -1,43 +1,45 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  dependencies = {
-    -- ts-autotag utilizes treesitter to understand the code structure to automatically close tsx tags
-    "windwp/nvim-ts-autotag"
-  },
-  build = ":TSUpdate",
-  enabled = true,
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "vim",
-        "vimdoc",
-        "java",
-        "javascript",
-        "typescript",
-        "lua",
-        "ruby",
-        "html",
-        "css",
-        "json",
-        "tsx",
-        "bash",
-        "markdown",
-        "markdown_inline",
-        "gitignore",
-        "xml",
-      },
-      indent = { enable = true },
-      highlight = {
-        enable = true,
-        use_languagetree = true,
-        -- disable = { "markdown" },
-      },
-      autotag = {
-        enable = true
-      },
-    })
-  end,
-  -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-  require("ts_context_commentstring").setup({}),
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPost", "BufNewFile" },
+	dependencies = {
+		-- ts-autotag utilizes treesitter to understand the code structure to automatically close tsx tags
+		"windwp/nvim-ts-autotag",
+	},
+	build = ":TSUpdate",
+	enabled = true,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"vim",
+				"vimdoc",
+				"java",
+				"javascript",
+				"typescript",
+				"lua",
+				"ruby",
+				"html",
+				"css",
+				"json",
+				"tsx",
+				"bash",
+				"markdown",
+				"markdown_inline",
+				"gitignore",
+				"xml",
+				"c",
+				"java",
+			},
+			indent = { enable = true },
+			highlight = {
+				enable = true,
+				use_languagetree = true,
+				-- disable = { "markdown" },
+			},
+			autotag = {
+				enable = true,
+			},
+		})
+	end,
+	-- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
+	require("ts_context_commentstring").setup({}),
 }
