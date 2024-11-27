6e2c61744b9244edc1e820576510db45d5f28521
Luciano Diamand <lucianodiamand@gmail.com>
2024-11-27 08:14:42 -0300

    Update tsserver to ts_ls

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -16,7 +16,7 @@ return {
 			mason_lspconfig.setup({
 				-- list of servers for mason to install
 				ensure_installed = {
-					"tsserver",
+					"ts_ls",
 					"html",
 					"angularls",
 					"cssls",
