46065f4f3adccffa0abc21f23ac1b40e0da7be89
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2024-12-29 21:49:22 -0300

    Change emacs default working directory

--- emacs/.emacs.d/modules/ldd-core.el
+++ emacs/.emacs.d/modules/ldd-core.el
@@ -1,5 +1,46 @@
 ;; -*- lexical-binding: t; -*-
 
+;; Change the user-emacs-directory to keep unwanted things out of ~/.emacs.d
+(setq user-emacs-directory (expand-file-name "~/.cache/emacs/")
+      url-history-file (expand-file-name "url/history" user-emacs-directory))
+
+;; Use no-littering to automatically set common paths to the new user-emacs-directory
+(use-package no-littering
+  :demand t
+  :config
+  ;; Set the custom-file to a file that won't be tracked by Git
+  (setq custom-file (if (boundp 'server-socket-dir)
+                        (expand-file-name "custom.el" server-socket-dir)
+                      (no-littering-expand-etc-file-name "custom.el")))
+  (when (file-exists-p custom-file)
+    (load custom-file t))
+
+  ;; Don't litter project folders with backup files
+  (let ((backup-dir (no-littering-expand-var-file-name "backup/")))
+    (make-directory backup-dir t)
+    (setq backup-directory-alist
+          `(("\\`/tmp/" . nil)
+            ("\\`/dev/shm/" . nil)
+            ("." . ,backup-dir))))
+
+  (setq auto-save-default nil)
+
+  ;; Tidy up auto-save files
+  (setq auto-save-default nil)
+  (let ((auto-save-dir (no-littering-expand-var-file-name "auto-save/")))
+    (make-directory auto-save-dir t)
+    (setq auto-save-file-name-transforms
+          `(("\\`/[^/]*:\\([^/]*/\\)*\\([^/]*\\)\\'"
+             ,(concat temporary-file-directory "\\2") t)
+            ("\\`\\(/tmp\\|/dev/shm\\)\\([^/]*/\\)*\\(.*\\)\\'" "\\3")
+            ("." ,auto-save-dir t)))))
+
+;; Silence compiler warnings as they can be pretty disruptive
+(setq native-comp-async-report-warnings-errors nil)
+
+;; Set the right directory to store the native comp cache
+(add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache/" user-emacs-directory))
+
 ;; Appearance
 
 ;; Solarized-theme
