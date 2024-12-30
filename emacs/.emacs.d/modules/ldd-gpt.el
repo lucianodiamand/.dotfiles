a46c17609357103f5f11107891447bd01912b9a4
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2024-12-30 12:16:33 -0300

    Refactor gptel to modules

---
+++ emacs/.emacs.d/modules/ldd-gpt.el
@@ -0,0 +1,14 @@
+;; -*- lexical-binding: t; -*-
+
+(use-package gptel
+             ;;:ensure t
+             :config
+             (setq auth-sources '("~/.authinfo"))
+             (setq gptel-api-key (auth-source-pick-first-password :host "api.openai.com"))
+             (setq gptel-model "gpt-3.5-turbo")
+             (setq qptel-max-tokens 1000)
+             (setq gptel-debug t)
+             (setq gptel-retry-on-error t)
+             (setq gptel-retry-delay 5))
+
+(provide 'ldd-gpt)
