b18245eb8e96ccb21d44bbb1a3a70316c7ce39dd
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2025-07-06 22:35:28 -0300

    Add local home-manager

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,17 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
-    homeConfigurations.user = home-manager.lib.homeManagerConfiguration {
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      modules = [ ./home.nix ];
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      homeConfigurations.user = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home.nix
+          {
+            home.username = "user";
+            home.homeDirectory = "/home/user";
+            home.stateVersion = "25.05";
+          }
+        ];
+      };
     };
-  };
 }
 
