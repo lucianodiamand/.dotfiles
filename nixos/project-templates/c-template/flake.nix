0eb39bf12dadaea81766de66a0f5edcfa7680032
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2025-07-10 16:22:58 -0300

    Fix flake version

--- nixos/project-templates/c-template/flake.nix
+++ nixos/project-templates/c-template/flake.nix
@@ -1,6 +1,6 @@
 {
   description = "C project template";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
--- nixos/project-templates/java-gradle-template/flake.nix
+++ nixos/project-templates/java-gradle-template/flake.nix
@@ -1,12 +1,12 @@
 {
   description = "Java Gradle template";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ jdk17 gradle ];
+          packages = with pkgs; [ jdk23 gradle ];
         };
       });
 }
