0166f7c0750d983f7eb30ee193ffeea0f498dd61
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2025-07-11 09:17:50 -0300

    Add zsh scripts

---
+++ zsh/bin/download_install_oracle_instantclient.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+INSTANT_CLIENT=instantclient-basic-linux.x64-23.8.0.25.04.zip
+
+wget -c https://download.oracle.com/otn_software/linux/instantclient/2380000/${INSTANT_CLIENT} -P /tmp
+sudo mkdir -p /opt/oracle
+sudo unzip /tmp/${INSTANT_CLIENT} -d /opt/oracle
