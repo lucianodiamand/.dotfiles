6606cf8bccc549b24fd2bc81e46d2e1ae20cafd7
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2025-07-11 09:01:41 -0300

    Upgrade version

--- bin/bin/download_install_oracle_instantclient.sh
+++ bin/bin/download_install_oracle_instantclient.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-INSTANT_CLIENT=instantclient-basic-linux.x64-23.4.0.24.05.zip
+INSTANT_CLIENT=instantclient-basic-linux.x64-23.8.0.25.04.zip
 
-wget -c https://download.oracle.com/otn_software/linux/instantclient/2340000/${INSTANT_CLIENT} -P /tmp
+wget -c https://download.oracle.com/otn_software/linux/instantclient/2380000/${INSTANT_CLIENT} -P /tmp
 sudo mkdir -p /opt/oracle
 sudo unzip /tmp/${INSTANT_CLIENT} -d /opt/oracle
