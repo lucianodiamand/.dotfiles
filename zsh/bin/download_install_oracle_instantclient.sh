fd2a350df9700234b6026192e13fb913e2e64d66
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2025-07-12 10:41:54 -0300

    Change directory to home

--- zsh/bin/download_install_oracle_instantclient.sh
+++ zsh/bin/download_install_oracle_instantclient.sh
@@ -3,5 +3,5 @@
 INSTANT_CLIENT=instantclient-basic-linux.x64-23.8.0.25.04.zip
 
 wget -c https://download.oracle.com/otn_software/linux/instantclient/2380000/${INSTANT_CLIENT} -P /tmp
-sudo mkdir -p /opt/oracle
-sudo unzip /tmp/${INSTANT_CLIENT} -d /opt/oracle
+mkdir -p ~/.oracle
+unzip /tmp/${INSTANT_CLIENT} -d ~/.oracle
