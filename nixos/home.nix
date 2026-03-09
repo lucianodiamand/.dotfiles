2010460840eaae80ea8508daff7c9779c20fc470
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2026-03-09 20:48:41 -0300

    Add w3m options

--- nixos/home.nix
+++ nixos/home.nix
@@ -206,6 +206,9 @@ in {
 
   # btop config
   home.file.".config/btop/btop.conf".source = "${dotfiles}/btop/.config/btop/btop.conf";
+ 
+  # w3m config
+  home.file.".w3m/config".source = "${dotfiles}/w3m/.w3m/config";
 
   home.file.".Xresources.d" = {
     source = "${dotfiles}/rxvt/.Xresources.d";  # carpeta en tu repo
