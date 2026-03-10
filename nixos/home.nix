d702311b09d2c0e570b93de231f5bc6a496f1a26
Luciano Diamand <lucianodiamand@gmail.com>
2026-03-10 12:38:03 -0300

    Add ssh configuration file

--- nixos/home.nix
+++ nixos/home.nix
@@ -4,7 +4,7 @@ let
   dotfiles = ../.;
   pass-otp = pkgs.pass.withExtensions (e: [ e.pass-otp ]);
 in {
-  imports = [ ./w3m.nix ./bat.nix ./btop.nix ./zathura.nix ./emacs.nix ./nvim.nix ];
+  imports = [ ./w3m.nix ./bat.nix ./btop.nix ./zathura.nix ./emacs.nix ./nvim.nix ./ssh.nix ];
   home.username = "user";
   home.homeDirectory = "/home/user";
   home.stateVersion = "25.05";
@@ -44,7 +44,6 @@ in {
     pass-otp
     #pinentry-tty
     pinentry-gtk2
-    openssh
     nodejs_22
     yarn
     python3
@@ -374,71 +373,6 @@ in {
     # dailies: bitacora
     mkdir -p ~/org/roam/{projects,tasks,notes,meetings,versions,dailies,slides,labs}
   '';
-  home.activation.generateHostingerKey = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    KEYFILE="$HOME/.ssh/bitbucket.l2.repo.key"
-    if [ ! -f "$KEYFILE" ]; then
-      echo "Generating SSH key for Bitbucket L2 repo..."
-      mkdir -p "$HOME/.ssh"
-      chmod 700 "$HOME/.ssh"
-      ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f "$KEYFILE" -C "Bitbucket L2 repo" -N ""
-      chmod 600 "$KEYFILE"
-    fi
-
-    KEYFILE="$HOME/.ssh/cpt-prod.server.key"
-    if [ ! -f "$KEYFILE" ]; then
-      echo "Generating SSH key for cpt server..."
-      mkdir -p "$HOME/.ssh"
-      chmod 700 "$HOME/.ssh"
-      ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f "$KEYFILE" -C "Donweb cpt vps server" -N ""
-      chmod 600 "$KEYFILE"
-    fi
-
-    KEYFILE="$HOME/.ssh/github.personal.repo.key"
-    if [ ! -f "$KEYFILE" ]; then
-      echo "Generating SSH key for personal Github repo..."
-      mkdir -p "$HOME/.ssh"
-      chmod 700 "$HOME/.ssh"
-      ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f "$KEYFILE" -C "Personal Github repo" -N ""
-      chmod 600 "$KEYFILE"
-    fi
-
-    KEYFILE="$HOME/.ssh/arq-desa.server.key"
-    if [ ! -f "$KEYFILE" ]; then
-      echo "Generating SSH key for arq desa server..."
-      mkdir -p "$HOME/.ssh"
-      chmod 700 "$HOME/.ssh"
-      ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f "$KEYFILE" -C "Arq desa server" -N ""
-      chmod 600 "$KEYFILE"
-    fi
-  '';
-
-  home.file.".ssh/config".text = ''
-    Host bitbucket-l2
-      HostName bitbucket.org
-      IdentityFile ~/.ssh/bitbucket.l2.repo.key
-      IdentitiesOnly yes
-
-    Host cptprod
-      HostName 179.43.117.86
-      IdentityFile ~/.ssh/cpt-prod.server.key
-      Port 4626
-
-    Host github.com
-      IdentityFile ~/.ssh/github.personal.repo.key
-
-    Host arqdesa
-      HostName 149.50.150.213
-      User root
-      IdentityFile ~/.ssh/arq-desa.server.key
-      Port 5961
-
-    Host sunde
-      HostName 192.168.122.191
-      HostKeyAlgorithms +ssh-rsa
-      PubkeyAcceptedAlgorithms +ssh-rsa
-      SetEnv TERM=rxvt
-  '';
-
   programs.git = {
     enable = true;
 
