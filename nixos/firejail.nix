398d5bcc2e9f6b78b5d5d429e37f5b251d92319d
Luciano Diamand <lucianodiamand@gmail.com>
2026-03-11 11:45:27 -0300

    Replace links with a copy

--- nixos/firejail.nix
+++ nixos/firejail.nix
@@ -1,10 +1,15 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 let
   dotfiles = ../.;
 in {
   home.packages = [ pkgs.btop ];
 
-  home.file.".config/firejail/firefox-bna.profile".source = "${dotfiles}/firejail/.config/firejail/firefox-bna.profile";
-  home.file.".config/firejail/bna.net".source = "${dotfiles}/firejail/.config/firejail/bna.net";
+  home.activation.firejailFiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    mkdir -p "$HOME/.config/firejail"
+    install -m 0644 "${dotfiles}/firejail/.config/firejail/firefox-bna.profile" \
+      "$HOME/.config/firejail/firefox-bna.profile"
+    install -m 0644 "${dotfiles}/firejail/.config/firejail/bna.net" \
+      "$HOME/.config/firejail/bna.net"
+  '';
 }
